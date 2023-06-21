709a925aa03bc4a0d5cd1e7331a823283c4e4a35
Stimarc <rikudo7474@gmail.net>
2023-06-21 21:18:34 +0300

    update

--- Todos/js/main.js
+++ Todos/js/main.js
@@ -1,54 +1,67 @@
 const doc = document;
 
-const
-    forms = doc.forms.addTodoForm,
-    formsEls = forms.elements,
-    addTodoInput = forms.addTodoInput,
-    addTodoBtn = forms.addTodoBtn;
+const forms = doc.forms.addTodoForm,
+  formsEls = forms.elements,
+  addTodoInput = forms.addTodoInput,
+  addTodoBtn = forms.addTodoBtn;
 
-    const todoListE1 = doc.querySelectorAll('todo-list');
+const todoListE1 = doc.querySelector('.todo-list');
 
 const todos = [
-    { id: 1, body: 'todo 1', completed: false },
-    { id: 1, body: 'todo 1', completed: false },
-    { id: 1, body: 'todo 1', completed: false },
+  { id: 1, body: 'todo 1', completed: false },
+  { id: 2, body: 'todo 2', completed: false },
+  { id: 3, body: 'todo 3', completed: false },
 ];
 
 renderTodoList(todos);
 
-// ->
 addTodoBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    const newTodoBody = addTodoInput.value;
+  event.preventDefault();
+  const newTodoBody = addTodoInput.value;
 
-    if (newTodoBody.trim() !== '') {
-        const newTodo = {
-            id: todos.length + 1,
-            body: newTodoBody,
-            completed: false
-        };
+  if (newTodoBody.trim() !== '') {
+    const newTodo = {
+      id: todos.length + 1,
+      body: newTodoBody,
+      completed: false
+    };
 
-        todos.push(newTodo);
-        console.log(todos);
+    todos.push(newTodo);
+    renderTodoList(todos);
 
-        addTodoInput.value = '';
-    }
-})
+    addTodoInput.value = '';
+  }
+});
 
+
+function deleteTodoItem(id) {
+  const index = todos.findIndex(todo => todo.id === id);
+  if (index !== -1) {
+    todos.splice(index, 1);
+    renderTodoList(todos);
+  }
+}
+
+todoListE1.addEventListener('click', function (event) {
+  if (event.target.classList.contains('action-btn-del')) {
+    const listItem = event.target.closest('.todo-item');
+    const todoId = parseInt(listItem.dataset.id);
+    deleteTodoItem(todoId);
+  }
+});
 
 function renderTodoList(todoList) {
-    const todoItemEls = todoList.map((todo, index) => `
-       <li class="todo-item">
-          <label class="todo_item_input">
-             <input type="checkbox" name="completed">
-          </label>
-          <p class="todo_item_text">${todo}</p>
-          <div class="todo-item-btns">
-             <button class="action-btn action-btn-del">x</button>
-          </div>
-       </li>
-    `);
- 
-    todoListE1.innerHTML = todoItemEls.join('');
- }
- 
+  const todoItemEls = todoList.map((todo, index) => `
+    <li class="todo-item" data-id="${todo.id}">
+      <label class="todo_item_input">
+        <input type="checkbox" name="completed">
+      </label>
+      <p class="todo_item_text">${todo.body}</p>
+      <div class="todo-item-btns">
+        <button class="action-btn action-btn-del">x</button>
+      </div>
+    </li>
+  `);
+
+  todoListE1.innerHTML = todoItemEls.join('');
+}
