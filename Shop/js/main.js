c91e814a4430dcf09cfe9b128dcaeedfdb4f1fc4
Stimarc <rikudo7474@gmail.net>
2023-07-07 03:12:52 +0300

    update Delete Shop

---
+++ Shop/js/main.js
@@ -0,0 +1,140 @@
+const doc = document;
+const productsSelector = '.products';
+const cart = {
+  1: 2,
+  2: 3,
+  3: 1,
+};
+
+renderProducts(products, productsSelector);
+renderCart(products, cart, 'body');
+
+function renderProducts(dataArr, insertSelector) {
+  for (let product of dataArr) {
+    renderProduct(product, insertSelector);
+  }
+}
+
+function renderProduct(prodObj, insertSelector) {
+  const parentEl = doc.querySelector(insertSelector);
+  const 
+    product = doc.createElement('div'),
+    productImgWrap = doc.createElement('div'),
+    productImg = doc.createElement('img'),
+    productTitle = doc.createElement('h3'),
+    productPriceBlock = doc.createElement('div'),
+    productPrice = doc.createElement('span'),
+    addCart = doc.createElement('button'),
+    productCategory = doc.createElement('span');
+
+  const {id, title, category, img, price} = prodObj;
+  const imgPath = `./img/products/${category}/${img}`;
+
+  if (!parentEl) {
+    console.error(`[${insertSelector}]: Parent element not found !!!`);
+    return false;
+  }
+
+  product.className = 'product';
+  product.dataset.id = id;
+
+  productImgWrap.className = 'product-img';
+  productImg.src = imgPath;
+  productImg.alt = img;
+  productImgWrap.append(productImg);
+
+  productTitle.className = 'product-title';
+  productTitle.innerHTML = title;
+
+  productPriceBlock.className = 'product-price-block';
+  productPrice.className = 'product-price';
+  productPrice.innerHTML = price;
+  
+  addCart.className = 'add-cart';
+  addCart.innerHTML = 'Add cart'
+  productPriceBlock.append(productPrice, addCart);
+
+  productCategory.className = 'product-category';
+  productCategory.innerText = category;
+
+  product.append(
+    productImgWrap,
+    productTitle,
+    productPriceBlock,
+    productCategory
+  );
+
+  parentEl.append(product);
+
+  // events
+  addCart.onclick = addCartHandler;
+}
+
+function renderCart(dataArr, cartProdsObj, insertSelector) {
+  const parentEl = doc.querySelector(insertSelector);
+
+  const 
+    cart = doc.createElement('div'),
+    cartTitle = doc.createElement('h3'),
+    cartProds = doc.createElement('ul');
+
+  if (!parentEl) {
+    console.error(`[${insertSelector}]: Parent element not found !!!`);
+    return false;
+  }
+
+  cart.className = 'cart';
+
+  cartTitle.className = 'cart-title';
+  cartTitle.innerText = 'Cart';
+
+  cartProds.className = 'cart-prods';
+
+  parentEl.append(cart);
+  cart.append(cartTitle, cartProds);
+
+  // render cart component
+  renderCartProds(dataArr, cartProdsObj, '.cart-prods');
+  renderCartTotal(5000, '.cart');
+}
+
+function renderCartProds(dataArr, cartProdsObj, insertSelector) {
+  let count = 1;
+
+  for (id in cartProdsObj) {
+    const qty = cartProdsObj[id];
+    const prod = dataArr.find(item => item.id == id);
+
+    renderCartProd(count,prod, qty,  insertSelector);
+    count ++;
+  }
+}
+
+function renderCartProd(count,prodObj, cartProdQty,   insertSelector) {
+  const parentEl = doc.querySelector(insertSelector);
+
+}
+
+function renderCartTotal(totalSum, insertSelector) {
+  const parentEl = doc.querySelector(insertSelector);
+
+  // console.log('render cart total');
+  // console.log(parentEl);
+}
+
+function GetParentElForRender(element) {
+  const parentEl = doc.querySelector(insertSelector);
+  if (!parentEl) {
+    console.error(`[${insertSelector}]: Parent element not found !!!`);
+    return false;
+  }
+
+  return parentEl;
+}
+
+// events
+function addCartHandler() {
+  const id = this.closest('.product').dataset.id;
+  
+  cart[id] = !cart[id] ? 1 : cart[id] + 1;
+}
---
+++ Shop/js/model.js
@@ -0,0 +1,9 @@
+const products = [
+  {id: 1, title: 'product1', category: 'notebooks', img: 'prod1.webp', price: 1500},
+  {id: 2, title: 'product6', category: 'smartphones', img: 'prod1.webp', price: 2000},
+  {id: 3, title: 'product5', category: 'monitors', img: 'prod1.webp', price: 1000},
+  {id: 4, title: 'product3', category: 'smartphones', img: 'prod2.webp', price: 1500},
+  {id: 5, title: 'product5', category: 'notebooks', img: 'prod2.webp', price: 2000},
+  {id: 6, title: 'product2', category: 'monitors', img: 'prod2.webp', price: 1000},
+];
+
