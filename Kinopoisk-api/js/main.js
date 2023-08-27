2aacf5a006326ea045699924135a04fbc2f1e4af
Stimarc <rikudo7474@gmail.net>
2023-08-27 15:55:55 +0300

    update fuck kinopoisk-api

--- Kinopoisk-api/js/main.js
+++ Kinopoisk-api/js/main.js
@@ -1,19 +1,85 @@
 const doc = document;
 
-const token = '5QT60PM-4AC4SHK-JD9RK3Y-T2P8275';
+const token = 'AG6ZB3T-N6N4CK6-K9YTRZ2-AFMG0BX';
 
 const baseUrl = 'https://api.kinopoisk.dev';
 const headers = {
     'accept': 'application/json'
-}
+};
 
 const resourses = {
     health: '/v1/health',
     movies: {
-        movie: '/v1.3/movie'
+        movie: '/v1.3/movie?page=2&limit=10'
+    }
+};
+
+const itemsPerPage = 10;
+let currentPage = 1;
+let sortBy = 'year'; 
+let data; 
+
+// 
+const prevBtn = doc.querySelector('.prev-btn');
+const nextBtn = doc.querySelector('.next-btn');
+
+prevBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        renderMovies(data.docs.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage), '.films');
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentPage < Math.ceil(data.docs.length / itemsPerPage)) {
+        currentPage++;
+        renderMovies(data.docs.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage), '.films');
+    }
+});
+
+// 
+const sortRadios = doc.querySelectorAll('[name="sort"]');
+sortRadios.forEach(radio => {
+    radio.addEventListener('change', event => {
+        sortBy = event.target.value;
+        data.docs.sort((a, b) => {
+            if (sortBy === 'year') return a.year - b.year;
+            if (sortBy === 'rating') return b.rating.kp - a.rating.kp;
+            if (sortBy === 'name') return a.name.localeCompare(b.name);
+        });
+        currentPage = 1;
+        renderMovies(data.docs.slice(0, itemsPerPage), '.films');
+    });
+});
+
+getMovies()
+    .then(movieData => {
+        data = movieData;
+        renderMovies(data.docs.slice(0, itemsPerPage), '.films');
+        data.docs.sort((a, b) => a.year - b.year);
+    });
+
+async function getMovies(page = 1) {
+    const url = `${baseUrl}${resourses.movies.movie}?page=${page}&limit=${itemsPerPage}`;
+    headers['X-API-KEY'] = token;
+
+    try {
+        const res = await fetch(url, { headers });
+        const data = await res.json();
+
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.warn('failed request', err);
     }
 }
 
+function renderMovies(filmsArr, parentElSelector) {
+    const parentEl = doc.querySelector(parentElSelector);
+    parentEl.innerHTML = ''; 
+
+    filmsArr.forEach(film => rendeMovie(film, parentElSelector));
+}
 
 function rendeMovie(filmObj, parentElSelector) {
     const parentEl = doc.querySelector(parentElSelector);
@@ -31,16 +97,15 @@ function rendeMovie(filmObj, parentElSelector) {
 
     const countriesHtmlEL = 
         countries
-            .map(country => `<li class="film__country">${ country.name }</li>`)
+            .map(country => `<li class="film__country">${country.name}</li>`)
             .join('');
 
     const genresHtmlEL = 
         genres
-            .map(genre => `<li class="film__genre">${ genre.name }</li>`)
+            .map(genre => `<li class="film__genre">${genre.name}</li>`)
             .join('');
 
     film.className = 'film';
-
     film.innerHTML =
     `
         <div class="film__front">
@@ -81,29 +146,21 @@ function rendeMovie(filmObj, parentElSelector) {
     parentEl.append(film);
 }
 
-async function getMovies(page, limit, sortBy) {
-    const url = baseUrl + `/v1.3/movie?page=${page}&limit=${limit}&sortBy=${sortBy}`;
+async function getMovies() {
+    const url = baseUrl + resourses.movies.movie;
+
     headers['X-API-KEY'] = token;
 
     try {
         const res = await fetch(url, { headers });
         const data = await res.json();
+
+        console.log(data);
         return data;
     } catch (err) {
         console.warn('failed request', err);
     }
 }
-
-function renderMovies(filmsArr, parentElSelector) {
-    filmsArr.forEach(film => rendeMovie(film, parentElSelector));
-}
-
-const currentPage = 1;
-const moviesPerPage = 10;
-const sortingCriteria = "rating"; 
-
-getMovies(currentPage, moviesPerPage, sortingCriteria)
-    .then(data => renderMovies(data.docs, '.films'));
 
 async function getHealth() {
     const url = baseUrl + resourses.health;
@@ -117,3 +174,4 @@ async function getHealth() {
     }
 
 }
+
