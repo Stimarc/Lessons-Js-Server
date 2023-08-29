f22dd9d1f767339b19802dd56efc95ed7000f7e8
Stimarc <rikudo7474@gmail.net>
2023-08-29 21:02:53 +0300

    update

---
+++ BOM/js/main.js
@@ -0,0 +1,27 @@
+// console.dir(window);
+// console.dir(document);
+
+// BOM
+console.dir(location);
+console.dir(history);
+// console.dir(navigation);
+// console.dir(navigator);
+console.dir(screen);
+
+
+
+// HISTORY
+btnBack.onclick = () => history.back();
+btnForward.onclick = () => history.forward();
+
+// WINDOW
+// const myWindow = window.open(
+// 'https://www.youtube.com/',
+// '_blank',
+// 'popup=yes width=300, height=300'
+// );
+
+// setTimeout(()=>{
+//     myWindow.close();
+// },5000);
+
---
+++ location/js/main.js
@@ -0,0 +1,48 @@
+const params = getSearchParams();
+
+const todos = [
+    { id: 2, body: 'todo2', completed: true },
+    { id: 1, body: 'todo1', completed: true },
+    { id: 4, body: 'todo4', completed: false },
+    { id: 5, body: 'todo5', completed: true },
+    { id: 6, body: 'todo6', completed: false },
+    { id: 3, body: 'todo3', completed: true },
+];
+
+const sortTodos = [...todos].sort((b, a) => {
+    return b.id - a.id;
+});
+
+let todosEls;
+
+console.log(params);
+
+params.sort
+    ? todosEls = getTodosEls(sortTodos)
+    : todosEls = getTodosEls(todos);
+
+todosParent.innerHTML = todosEls;
+
+// FUNCTIONS
+function getSearchParams() {
+    const p = location.search
+        .substring(1)
+        .split('&')
+        .map(item => item.split('='));
+
+
+    const params = Object.fromEntries(p);
+
+    return params;
+}
+
+function getTodosEls(data) {
+    return data
+        .map(todo => (
+            `<li class="todo">
+    <span>${todo.id}</span>
+    <span>${todo.body}</span>
+    <span>${todo.completed}</span>
+    </li>`))
+        .join('');
+}
