3271629516112a27b6331851746ac85b6fad9548
Stimarc <rikudo7474@gmail.net>
2023-08-31 17:50:44 +0300

    update

--- location/js/main.js
+++ location/js/main.js
@@ -1,48 +1,49 @@
+console.log(location);
+
 const params = getSearchParams();
 
 const todos = [
-    { id: 2, body: 'todo2', completed: true },
-    { id: 1, body: 'todo1', completed: true },
-    { id: 4, body: 'todo4', completed: false },
-    { id: 5, body: 'todo5', completed: true },
-    { id: 6, body: 'todo6', completed: false },
-    { id: 3, body: 'todo3', completed: true },
+  {id: 2, body: 'todo2', completed: true},
+  {id: 1, body: 'todo1', completed: true},
+  {id: 4, body: 'todo4', completed: true},
+  {id: 5, body: 'todo5', completed: false},
+  {id: 6, body: 'todo6', completed: true},
+  {id: 3, body: 'todo3', completed: false},
 ];
 
 const sortTodos = [...todos].sort((b, a) => {
-    return b.id - a.id;
+  return b.id - a.id;
 });
 
 let todosEls;
 
 console.log(params);
 
-params.sort
-    ? todosEls = getTodosEls(sortTodos)
-    : todosEls = getTodosEls(todos);
+params.sort 
+  ? todosEls = getTodosEls(sortTodos)
+  : todosEls = getTodosEls(todos);
 
 todosParent.innerHTML = todosEls;
 
 // FUNCTIONS
 function getSearchParams() {
-    const p = location.search
-        .substring(1)
-        .split('&')
-        .map(item => item.split('='));
+  const p = location.search
+    .substring(1)
+    .split('&')
+    .map(item => item.split('='));
 
+  const params = Object.fromEntries(p);
 
-    const params = Object.fromEntries(p);
-
-    return params;
+  return params;
 }
 
 function getTodosEls(data) {
-    return data
-        .map(todo => (
-            `<li class="todo">
-    <span>${todo.id}</span>
-    <span>${todo.body}</span>
-    <span>${todo.completed}</span>
+  return data
+  .map(todo => (
+    `<li class="todo">
+      <span>${todo.id}</span>
+      <span>${todo.body}</span>
+      <span>${todo.completed}</span>
     </li>`))
-        .join('');
+  .join('');
 }
